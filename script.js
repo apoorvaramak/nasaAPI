350c7d93c1b8aefeddee56c5dcccb0b8970a0393
Apoorva Ramakrishnan <apoorvar@comcast.net>
2020-11-25 15:38:07 -0500

    adding css + editing API

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 var app = {
-	nyTimesArticles : [],
+	nasaImages : [],
 
 	initialize: function() {
-		app.getNYTimesData();
+		app.getNasaImages();
 	},
 
 	makeHTML: function() {
 		var theHTML = '';
-		for (var i = 0; i < app.nyTimesArticles.length; i++){
+		for (var i = 0; i < app.nasaImages.length; i++){
 			theHTML += "<div class='nytArticle'>";
-			theHTML += "<h3>" + app.nyTimesArticles[i].headline.main + "</h3>";
+			theHTML += "<h3>" + app.nasaImages[i].collection.items[0].data[0].center + "</h3>";
 			theHTML += "</div>";
 		}
 		$('.container').html(theHTML);
 	},
 
-	getNYTimesData: function() {
-		console.log("Get NY Times Data");
+	getNasaImages: function() {
+		console.log("Get NASA Images/Videos");
 		var currentSearchWord = 'apollo%11';
-		var nyTimesURL = 'https://images-api.nasa.gov/search?q=' + currentSearchWord + '&api_key=';
-		var myNYKey = 'itOZtnn2XzP0a3GcrCaqH02bSM04rmEwQbhwpGRU';
-		var nasaURL = 'https://images-api.nasa.gov/search?q=' + currentSearchWord
-		var nyTimesReqURL = nyTimesURL + myNYKey;
+		//var nyTimesURL = 'https://images-api.nasa.gov/search?q=' + currentSearchWord + '&api_key=';
+		//var myNYKey = 'itOZtnn2XzP0a3GcrCaqH02bSM04rmEwQbhwpGRU';
+		var nasaURL = 'https://images-api.nasa.gov/search?q=' + currentSearchWord;
+		//var nyTimesReqURL = nyTimesURL + myNYKey;
 		console.log(nasaURL);
 		fetch(nasaURL)
 		.then(response => response.json())
 		.then(data => {
 			//;
-			app.nyTimesArticles = collection.items.data[0].center;
-			console.log(app.nyTimesArticles);
+			debugger;
+			app.nasaImages = data.collection.items[0].data[0].center;
+			console.log(app.nasaImages);
 			app.makeHTML();
 		})
 		.catch(error => console.log(error));

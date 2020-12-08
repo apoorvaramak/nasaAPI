7c89df53706eb5b2bdb633b4eea4041aca6c22f6
Apoorva Ramakrishnan <apoorvar@comcast.net>
2020-12-07 21:11:33 -0500

    making changes

--- script.js
+++ script.js
@@ -4,24 +4,30 @@ var app = {
 
 	initialize: function() {
 		document.getElementById("submitButton").addEventListener("click", function(){app.getNasaImages()})
+		document.getElementById("submitButton2").addEventListener("click", function(){app.getNasaImages()})
 	},
 
 	makeHTML: function() {
 		var theHTML = '';
-		debugger;
+		//debugger;
+		$("#form").addClass('hide');
 		var i;
 		for (i = 0; i < Object.keys(app.nasaImages).length; i++){
 			theHTML += "<div class='nasaTitle'>";
-			theHTML += "<h3>" + app.nasaImages.title + "</h3>";
+			theHTML += "<h3>" + "<a href=" + app.nasaImages[i].href + ">" + app.nasaImages[i].data[0].media_type.toUpperCase() + "</a>" + ": " + app.nasaImages[i].data[0].title + "</br>" + "</br>" + "<span>" + app.nasaImages[i].data[0].description + "</span>" + "</h3>";
+			// theHTML += "<h3>" + app.nasaImages[i].data[0].description + "</h3>";
 			theHTML += "</div>";
+
+
 		}
 		$('.container').html(theHTML);
+
 	},
 
 	getNasaImages: function() {
 		console.log("Get NASA Images/Videos");
 		var currentSearchWord = document.getElementById('myInput').value;
-		debugger;
+		//debugger;
 		//var nyTimesURL = 'https://images-api.nasa.gov/search?q=' + currentSearchWord + '&api_key=';
 		//var myNYKey = 'itOZtnn2XzP0a3GcrCaqH02bSM04rmEwQbhwpGRU';
 		var nasaURL = 'https://images-api.nasa.gov/search?q=' + currentSearchWord;
@@ -32,7 +38,7 @@ var app = {
 		.then(data => {
 			//;
 			//debugger;
-			app.nasaImages = data.collection.items[0].data[0];
+			app.nasaImages = data.collection.items;
 			console.log(app.nasaImages.description);
 			app.makeHTML();
 		})
